442e6c5ba4b30bfe7e1a7fdb17231349eece81f9
will <2623556566@qq.com>
2023-11-20 14:58:29 +0800

    asd

--- src/bag_to_pcd.cpp
+++ src/bag_to_pcd.cpp
@@ -15,7 +15,8 @@ string lidar_topic;
 string pcd_file;
 bool is_custom_msg;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "lidarCamCalib");
   ros::NodeHandle nh;
   nh.param<string>("bag_file", bag_file, "");
@@ -25,27 +26,34 @@ int main(int argc, char **argv) {
   pcl::PointCloud<pcl::PointXYZI> output_cloud;
   std::fstream file_;
   file_.open(bag_file, ios::in);
-  if (!file_) {
+  if (!file_)
+  {
     std::string msg = "Loading the rosbag " + bag_file + " failue";
     ROS_ERROR_STREAM(msg.c_str());
     return -1;
   }
   ROS_INFO("Loading the rosbag %s", bag_file.c_str());
   rosbag::Bag bag;
-  try {
+  try
+  {
     bag.open(bag_file, rosbag::bagmode::Read);
-  } catch (rosbag::BagException e) {
+  }
+  catch (rosbag::BagException e)
+  {
     ROS_ERROR_STREAM("LOADING BAG FAILED: " << e.what());
     return -1;
   }
   std::vector<string> lidar_topic_vec;
   lidar_topic_vec.push_back(lidar_topic);
   rosbag::View view(bag, rosbag::TopicQuery(lidar_topic_vec));
-  for (const rosbag::MessageInstance &m : view) {
-    if (is_custom_msg) {
+  for (const rosbag::MessageInstance &m : view)
+  {
+    if (is_custom_msg)
+    {
       livox_ros_driver::CustomMsg livox_cloud_msg =
           *(m.instantiate<livox_ros_driver::CustomMsg>()); // message type
-      for (uint i = 0; i < livox_cloud_msg.point_num; ++i) {
+      for (uint i = 0; i < livox_cloud_msg.point_num; ++i)
+      {
         pcl::PointXYZI p;
         p.x = livox_cloud_msg.points[i].x;
         p.y = livox_cloud_msg.points[i].y;
@@ -53,14 +61,17 @@ int main(int argc, char **argv) {
         p.intensity = livox_cloud_msg.points[i].reflectivity;
         output_cloud.points.push_back(p);
       }
-    } else {
+    }
+    else
+    {
       sensor_msgs::PointCloud2 livox_cloud;
       livox_cloud = *(m.instantiate<sensor_msgs::PointCloud2>()); // message
       pcl::PointCloud<pcl::PointXYZI> cloud;
       pcl::PCLPointCloud2 pcl_pc;
       pcl_conversions::toPCL(livox_cloud, pcl_pc);
       pcl::fromPCLPointCloud2(pcl_pc, cloud);
-      for (uint i = 0; i < cloud.size(); ++i) {
+      for (uint i = 0; i < cloud.size(); ++i)
+      {
         output_cloud.points.push_back(cloud.points[i]);
       }
     }
